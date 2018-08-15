a8db192e5e00955d3fb8b0a2e75dd10af133f944
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-08-16 02:44:29 +0530

    control: init roommap portal

--- routes/admin/portals/control/index.js
+++ routes/admin/portals/control/index.js
@@ -8,12 +8,14 @@ let manageAdmins = require("./manageAdmins.js");
 let portalToggle = require("./portals.js");
 let switchUser = require("./switchUser.js");
 let ttExceptions = require("./ttExceptions");
+var roomMap = require("./room-map.js")
 
 router.use("/holidays", holiday);
 router.use("/manage-admins", manageAdmins);
 router.use("/portal-toggle", portalToggle);
 router.use("/switch-user", switchUser);
 router.use("/tt-exceptions", ttExceptions);
+router.use("/room-map",roomMap);
 
 router.get("/", function(req, res, next) {
   res.renderState("admin/portals/control");
